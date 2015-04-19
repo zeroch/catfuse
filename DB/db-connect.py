a3818b9b31cf3bd1fafa39ad679588212f09dc57
Natchaphon Ruengsakulrach <nruengsa@gatech.edu>
2015-04-19 16:54:44 -0400

    Added dbREQ fucntion
    dbReq function sends the request message containing IP address of the replica and the requested to the master node

--- DB/db-connect.py
+++ DB/db-connect.py
@@ -57,6 +57,40 @@ def dbLIST():
 		return "LIST_ERROR"
 	return msg			
 
+def dbREQ(requestFile,clientSock,clientAddr):
+	requestList = requestFile.split(":")
+	strReq = ""
+
+	for reqfile in requestList:
+		strReq = strReq + "," + reqfile
+		
+	reqMSG = clientAddr[0] + strReq
+	thread.start_new_thread(contactMasterNode,(reqMSG,))
+
+	return "REQUEST_OK"
+
+def contactMasterNode(reqMSG):
+	master = "localhost"
+	port = 12346
+	buf = 1234
+
+	clientSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	clientSock.connect((master,port))
+
+	while True:
+		try:
+			clientSock.send(reqMSG)
+			recvdata = clientSock.recv(buf)
+			if not recvdata:
+				break
+			else:
+				print recvdata
+		except socket.error,e:
+			print "Cannot contact the master node"
+			break
+
+	clientSock.close()
+
 def clientHandler(clientSock,clientAddr):
 	try:
 		query = clientSock.recv(1024).split(",")
@@ -72,7 +106,9 @@ def clientHandler(clientSock,clientAddr):
 	elif int(query[0]) == 3: # LIST
 		msg = dbLIST()
 		if len(msg) == 0:
-			msg = "EMPTY" 	
+			msg = "EMPTY"
+	elif int(query[0]) == 4: # REQUEST_FILE
+		msg = dbREQ(query[1],clientSock,clientAddr) 	
 	else:
 		msg = "Invalid Query"
 
