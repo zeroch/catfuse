3e8ae324039c97973f08c991a5623ba041f73e22
ze chen <chenze62@gmail.com>
2015-04-20 23:51:27 -0700

    Master: kick-start database connection

--- Master/thread_server.c
+++ Master/thread_server.c
@@ -100,3 +100,44 @@ int socket_init(void )
     }
 
 }
+
+
+
+int kick_start(void )
+{
+    int clientSocket;
+    char buffer[2000];
+    char read_buf[2000];
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
+
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(DB_PORT);
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr(DB_IP);
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+    /*---- Connect the socket to the server using the address struct ----*/
+    addr_size = sizeof serverAddr;
+    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+    /*---- Read the message from the server into the buffer ----*/
+
+    strcpy(buffer, "6,   / I am walking up!");
+    send(clientSocket, buffer, 2000, 0);
+    recv(clientSocket, read_buf, 2000, 0);
+
+    /*---- Print the received message ----*/
+    printf("Data received: %s",buffer);   
+
+    printf("------------ Kick starting DataBase -------\n");
+
+}
